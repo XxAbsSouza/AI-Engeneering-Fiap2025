2fc7618b0773aea0233eba39c9b2a37ca77c0d3d
Ana Beatriz  dos Santos Souza <beatriz_ana58@hotmail.com>
2025-02-20 22:26:06 -0300

    deu certooooooooooo

--- Lab3_Aula2002/teste2.py
+++ Lab3_Aula2002/teste2.py
@@ -1,8 +1,8 @@
 import cv2
 
 # Iniciando a captura de vídeo
-cap = cv2.VideoCapture(0) #webcam
-#cap = cv2.VideoCapture("Video.mp4") #video que foi salvo
+#cap = cv2.VideoCapture(0) #webcam
+cap = cv2.VideoCapture("totoro.mp4") #video que foi salvo
 
 while True:
     # Tenta fazer a Captura do frame
@@ -14,11 +14,14 @@ while True:
         break
     
     # processa o frame capturado
+    # normalFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     # Exibe o frame processado
+    # cv2.imshow('frame', normalFrame)
     cv2.imshow('frame', frame)
     cv2.imshow('frame_processado', gray)
+
     # Aguarda 1 ms e verifica se a tecla 'q' foi pressionada para sair
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
