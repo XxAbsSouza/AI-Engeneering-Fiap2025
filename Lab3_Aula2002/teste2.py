c089342b8628e9a49c4c6815207a425ea31ae6c0
Ana Beatriz  dos Santos Souza <beatriz_ana58@hotmail.com>
2025-02-25 11:57:25 -0300

    ..

--- Lab3_Aula2002/teste2.py
+++ Lab3_Aula2002/teste2.py
@@ -1,31 +1,53 @@
 import cv2
 
-# Iniciando a captura de vídeo
-#cap = cv2.VideoCapture(0) #webcam
-cap = cv2.VideoCapture("totoro.mp4") #video que foi salvo
+# # Iniciando a captura de vídeo
+# cap = cv2.VideoCapture(0) #webcam
+# cap2 = cv2.VideoCapture("Lab3_Aula2002/totoro.mp4") #video que foi salvo
 
-while True:
-    # Tenta fazer a Captura do frame
-    ret, frame = cap.read()
+# while True:
+#     # Tenta fazer a Captura do frame
+#     ret, frame = cap.read()
+#     ret, frame2 = cap2.read()
 
-    # verifica se o frame foi capturado corretamente
+#     # verifica se o frame foi capturado corretamente
+#     if not ret:
+#         print("Erro: Não foi possível capturar o frame.")
+#         break
+    
+#     # processa o frame capturado
+#     normalFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    
+#     # Exibe o frame processado
+#     cv2.imshow('camera', frame)
+#     cv2.imshow('camera_processado', normalFrame)
+#     cv2.imshow('frame', frame2)
+#     cv2.imshow('frame_processado', gray)
+
+#     # Aguarda 1 ms e verifica se a tecla 'q' foi pressionada para sair
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+# # Libera a captura e fecha todas as janelas
+# cap.release()
+# cv2.destroyAllWindows()
+
+import matplotlib.pyplot as plt
+
+video_path = "Lab3_Aula2002/video.mp4"
+video = cv2.VideoCapture(video_path)
+
+fig, ax = plt.subplots()
+
+while video.isOpened():
+    ret, frame = video.read()
     if not ret:
-        print("Erro: Não foi possível capturar o frame.")
         break
     
-    # processa o frame capturado
-    # normalFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    # Exibe o frame processado
-    # cv2.imshow('frame', normalFrame)
-    cv2.imshow('frame', frame)
-    cv2.imshow('frame_processado', gray)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Converte para RGB
+    ax.clear()
+    ax.imshow(frame)
+    plt.pause(0.03)  # Pequena pausa para atualização
 
-    # Aguarda 1 ms e verifica se a tecla 'q' foi pressionada para sair
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Libera a captura e fecha todas as janelas
-cap.release()
-cv2.destroyAllWindows()
+video.release()
+plt.close()
